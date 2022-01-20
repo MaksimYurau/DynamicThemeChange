6adfb66d5895925b361caee034587dde5110ae9c
MaksimYurau <2004softwaredeveloper@gmail.com>
2022-01-20 22:39:08 +0300

    Updated project. Added ColorChangeActivity.kt. Added activity_color_change.xml. Added styles.xml. Updated gradle.xml. Updated MainActivity.kt. Updated activity_main.xml. Updated strings.xml. Updated AndroidManifest.xml.

---
+++ app/src/main/java/com/example/android/dynamicthemechange/ColorChangeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android.dynamicthemechange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ColorChangeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_color_change)
+    }
+}
--- app/src/main/java/com/example/android/dynamicthemechange/MainActivity.kt
+++ app/src/main/java/com/example/android/dynamicthemechange/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.android.dynamicthemechange
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mButtonToColorChangeActivity: Button
+    private lateinit var mFirstTextView: TextView
+    private lateinit var mSecondTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initializeViews()
+
+        setOnGoToColorChangeActivityButtonClickListener()
+    }
+
+    private fun initializeViews() {
+        mButtonToColorChangeActivity = findViewById(R.id.go_to_color_change_activity_button)
+        mFirstTextView = findViewById(R.id.first_textView)
+        mSecondTextView = findViewById(R.id.second_textView)
+    }
+
+    private fun setOnGoToColorChangeActivityButtonClickListener() {
+        mButtonToColorChangeActivity.setOnClickListener {
+            val intentToColorChangeActivity =
+                Intent(this@MainActivity, ColorChangeActivity::class.java)
+            startActivity(intentToColorChangeActivity)
+        }
     }
 }
